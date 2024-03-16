e4ddc92a0d94d5a49557798b5adf9455ec6eaff0
Andrew Cheah <62257557+atcheah@users.noreply.github.com>
2024-03-16 14:24:32 -0700

    cleaned up logs

--- src/ai_system/ai_system.cpp
+++ src/ai_system/ai_system.cpp
@@ -15,14 +15,16 @@
 // coordinate system details
 // world is 960x960
 // each cell is 64x64 so there are 15x15 cells
-// the top left cell is (0,0) and the bottom right cell is (12,12)
+// the top left cell is (0,0) and the bottom right cell is (14,14)
 // the world is placed in the middle of the screen (where the screen is 1920x1024)
-// so the top left corner of the world is at (480, 32)
+// so the top left corner of the world is at (480, 32) and the bottom right corner is at (1440, 992)
 const float cellSize = 64.0f;
 const int numCells = 15;
 const int maxIterations = 1000;
-const float xOffSet = 480.0f; 
-const float yOffSet = 32.0f;
+const float xMin = 480.0f; 
+const float yMin = 32.0f;
+const float xMax = 1440.0f;
+const float yMax = 992.0f;
 
 struct Vec2Hash {
     std::size_t operator()(const vec2& v) const {
@@ -35,20 +37,23 @@ bool AISystem::lineOfSightClear(const vec2& start, const vec2& end) {
     // Amanatides, J., & Woo, A. (1987). A fast voxel traversal algorithm for ray tracing. Eurographics, 87(3), 3-10.
     // This is a simple implementation of the Bresenham's line algorithm
 
-    int cellX = static_cast<int>(floor((start.x - xOffSet) / cellSize));
-    int cellY = static_cast<int>(floor((start.y - yOffSet) / cellSize));
+    int x = static_cast<int>(start.x - xMin);
+    int cellX = static_cast<int>(floor((x / cellSize)));
+    int y = static_cast<int>(start.y - yMin);
+    int cellY = static_cast<int>(floor((y/ cellSize)));
+    assert(xMin <= x <= xMax && yMin <= y <= yMax);
+    assert(0 <= cellX <= numCells - 1 && 0 <= cellY <= numCells - 1);
 
-    int endCellX = static_cast<int>(floor((end.x - xOffSet) / cellSize));
-    int endCellY = static_cast<int>(floor((end.y - yOffSet) / cellSize));
-
-    assert(0 <= cellX <= numCells && 0 <= cellY <= numCells);
+    int endX = static_cast<int>(end.x - xMin);
+    int endCellX = static_cast<int>(floor((endX / cellSize)));
+    int endY = static_cast<int>(end.y - yMin);
+    int endCellY = static_cast<int>(floor((endY / cellSize)));
+    assert(xMin <= endX <= xMax && yMin <= endY <= yMax);
+    assert(0 <= endCellX <= numCells - 1 && 0 <= endCellY <= numCells - 1);
 
     vec2 direction = end - start;
     int stepX = (direction.x > 0) ? 1 : -1;
     int stepY = (direction.y > 0) ? 1 : -1;
-
-    float boundaryX = cellX * cellSize + ((stepX > 0) ? cellSize : 0);
-    float boundaryY = cellY * cellSize + ((stepY > 0) ? cellSize : 0);
 
     float deltaT_X = fabs(cellSize / direction[0]);
     float deltaT_Y = fabs(cellSize / direction[1]);
@@ -56,19 +61,13 @@ bool AISystem::lineOfSightClear(const vec2& start, const vec2& end) {
     float remainingT_X = deltaT_X * (1 - fmod(start.x, cellSize));
     float remainingT_Y = deltaT_Y * (1 - fmod(start.y, cellSize));
 
-    std::cout << "start: " << start.x << ", " << start.y << std::endl;
-    std::cout << "start-cell: " << cellX << ", " << cellY << std::endl;
-    std::cout << "end: " << end.x << ", " << end.y << std::endl;
-    std::cout << "end-cell: " << endCellX << ", " << endCellY << std::endl;
-
-    
    for (int i = 0; i < maxIterations; i++) {
 		if (remainingT_X < remainingT_Y) {
-			cellX += stepX;
+            cellX += stepX;
 			remainingT_X += deltaT_X;
 		}
 		else {
-			cellY += stepY;
+            cellY += stepY;
 			remainingT_Y += deltaT_Y;
 		}
 
@@ -82,14 +81,7 @@ bool AISystem::lineOfSightClear(const vec2& start, const vec2& end) {
         if (cellX == endCellX && cellY == endCellY) {
             break;
 		}
-
-        boundaryX = deltaT_X * stepX;
-        boundaryY = deltaT_Y * stepY;
-
-        //std::cout << "-> " << cellX << ", " << cellY << std::endl;
-
 	}
-
    return true;
 }
 
@@ -99,7 +91,6 @@ bool AISystem::isObstacleAtPosition(const vec2& position) {
     // check if there is an obstacle at the given position
     // NEED A WAY TO BE TOLD THE CURENT ROOM
     // once has would something along these lines
-
    /* Room& room = registry.rooms.get(currentRoom);
 
     for (auto& pos : room.obstacle_positions) {
@@ -237,43 +228,41 @@ void AISystem:: idleState(Entity entity, Motion& motion) {
 void AISystem::activeState(Entity entity, Motion& motion, float elapsed_ms) {
     vec2 playerPosition = registry.motions.get(registry.players.entities[0]).position;
 
-    // Check for line of sight for both ranged and melee types
-    if (lineOfSightClear(motion.position, playerPosition)) {
-        // Rotate towards player for both melee and ranged if in line of sight
-        vec2 direction = normalize(playerPosition - motion.position);
-        float angle = atan2(direction.y, direction.x);
-        motion.look_angle = angle; // Rotate towards player
+    //// Check for line of sight for both ranged and melee types
+    //if (lineOfSightClear(motion.position, playerPosition)) {
+    //    // Rotate towards player for both melee and ranged if in line of sight
+    //    vec2 direction = normalize(playerPosition - motion.position);
+    //    float angle = atan2(direction.y, direction.x);
+    //    motion.look_angle = angle; // Rotate towards player
 
-        AI& ai = registry.ais.get(entity);
-
-        if (ai.type == AI::AIType::RANGED) {
+    AI& ai = registry.ais.get(entity);
+    switch (ai.type) {
+        case AI::AIType::RANGED:
             // Ranged AI behavior
             handleRangedAI(entity, motion, ai, elapsed_ms, playerPosition);
-        }
-        //else if (ai.type == AI::AIType::MELEE) {
-        //    // Melee AI behavior using A* for pathfinding
-        //    std::vector<vec2> path = findPathAStar(motion.position, playerPosition);
-        //    if (path.size() > 1) { // Ensure path has more than just the starting position
-        //        vec2 nextStep = path[1]; // Get the next step towards the player
+            break;
+        case AI::AIType::MELEE:
+            //handleMeleeAI(entity, motion, ai, elapsed_ms, playerPosition);
+            // Melee AI behavior using A* for pathfinding
+            //    std::vector<vec2> path = findPathAStar(motion.position, playerPosition);
+            //    if (path.size() > 1) { // Ensure path has more than just the starting position
+            //        vec2 nextStep = path[1]; // Get the next step towards the player
 
-        //        direction = normalize(nextStep - motion.position);
-        //        motion.look_angle = atan2(direction.y, direction.x);
-        //        float speed = 10.0f; // Define a suitable speed for the melee enemy
+            //        direction = normalize(nextStep - motion.position);
+            //        motion.look_angle = atan2(direction.y, direction.x);
+            //        float speed = 10.0f; // Define a suitable speed for the melee enemy
 
-        //        // Update velocity towards the next step in the path
-        //        motion.velocity = direction * speed;
-        //    }
-        //}
-    }
-    else {
-        // If there's no line of sight, stop movement
-        motion.velocity = vec2(0.0f, 0.0f);
+            //        // Update velocity towards the next step in the path
+            //        motion.velocity = direction * speed;
+            //    }
+            break;
+        default:
+            break;
     }
 }
 
 void AISystem::handleRangedAI(Entity entity, Motion& motion, AI& ai, float elapsed_ms, const vec2& playerPosition) {
     float distanceToPlayer = length(playerPosition - motion.position);
-
     if (lineOfSightClear(motion.position, playerPosition)) {
         // Rotate towards the player
         vec2 direction = normalize(playerPosition - motion.position);
@@ -286,13 +275,13 @@ void AISystem::handleRangedAI(Entity entity, Motion& motion, AI& ai, float elaps
         else {
             // Within safe distance, stop moving
             motion.velocity = vec2(0.0f, 0.0f);
+        }
 
-            // Shooting logic with cooldown
-            ai.shootingCooldown -= elapsed_ms / 1000.0f; // Convert milliseconds to seconds
-            if (ai.shootingCooldown <= 0) {
-                createProjectileForEnemy(motion.position, motion.look_angle + M_PI/2, entity);
-                ai.shootingCooldown = 1.0f; // Reset cooldown to 2.5 seconds or appropriate value
-            }
+        // Shooting logic with cooldown
+        ai.shootingCooldown -= elapsed_ms / 1000.0f; // Convert milliseconds to seconds
+        if (ai.shootingCooldown <= 0) {
+            createProjectileForEnemy(motion.position, motion.look_angle + M_PI/2, entity);
+            ai.shootingCooldown = 1.0f; // Reset cooldown to 2.5 seconds or appropriate value
         }
     }
     else {
