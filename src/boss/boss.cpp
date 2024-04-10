b3b007a1326d65cca31b9d99c56e457b86ec4d28
SHAFQUAT UL BARI <sbarry11@students.cs.ubc.ca>
2024-04-10 02:36:09 -0700

    fixed looping animation shield

--- src/boss/boss.cpp
+++ src/boss/boss.cpp
@@ -160,7 +160,7 @@ void Boss::handleOffensiveState(Entity entity, BossAI& boss, Motion& motion, flo
         animation.sheet_id = SPRITE_SHEET_ID::ENEMY_BOSS_SHIELD;
         animation.total_frames = 19;
         animation.sprites = { {0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}, {7, 0}, {8, 0}, {9, 0}, {10, 0}, {11, 0}, {12, 0}, {13, 0}, {14, 0}, {15, 0}, {16, 0}, {17, 0}, {18, 0} };
-        animation.frame_durations_ms = { 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 };
+        animation.frame_durations_ms = { 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, INFINITY};
         animation.loop = true;
         animation.current_frame = 0;
         AnimationTimer& animation_timer = registry.animationTimers.get(entity);
--- src/world_init/world_init.cpp
+++ src/world_init/world_init.cpp
@@ -217,7 +217,7 @@ Entity createBoss(RenderSystem* renderer, vec2 position, float health_points, Bo
 	case BossAI::BossState::DEFENSIVE:
 		animation.sheet_id = SPRITE_SHEET_ID::ENEMY_BOSS_SHIELD;
 		animation.sprites = { {0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}, {7, 0}, {8, 0}, {9, 0}, {10, 0}, {11, 0}, {12, 0}, {13, 0}, {14, 0}, {15, 0}, {16, 0}, {17, 0}, {18, 0} };
-		animation.frame_durations_ms = { 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 };
+		animation.frame_durations_ms = { 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, INFINITY };
 		// Add frames and durations for DEFENSIVE state
 		break;
 	case BossAI::BossState::OFFENSIVE:
